96481e67aa323158f9beb05d23fe420fc8170ebe
WhatsARanjit <whatsaranjit@gmail.com>
2017-11-02 23:05:51 +0000

    Updating some indentation

--- lib/magicbox/checks/fact.rb
+++ lib/magicbox/checks/fact.rb
@@ -1,19 +1,19 @@
 module Magicbox::Checks
   class Fact < Magicbox::Check
     def parse
-    begin
-      code  = URI.unescape(@data['code']).chomp
-      fact  = @data['fact'].chomp
-      value = @data['value'].is_a?(String) ? URI.unescape(@data['value']).chomp : @data['value']
-      require 'facter'
-      Facter.clear
-      parse = eval(code).value
-      exitstatus = parse == value ? 0 : 1
-    rescue => e
-      { "exitcode" => 1, "message" => [e.message]}.to_json
-    else
-      { "exitcode" => exitstatus, "message" => ["expected: #{value}", "actual: #{parse}"].flatten(1)}.to_json
-    end
+      begin
+        code  = URI.unescape(@data['code']).chomp
+        fact  = @data['fact'].chomp
+        value = @data['value'].is_a?(String) ? URI.unescape(@data['value']).chomp : @data['value']
+        require 'facter'
+        Facter.clear
+        test_fact  = eval(code)
+        exitstatus = Facter.value(fact) == value ? 0 : 1
+      rescue => e
+        { "exitcode" => 1, "message" => [e.message]}.to_json
+      else
+        { "exitcode" => exitstatus, "message" => ["expected: #{value}", "actual: #{Facter.value(fact)}"].flatten(1)}.to_json
+      end
     end
   end
 end
