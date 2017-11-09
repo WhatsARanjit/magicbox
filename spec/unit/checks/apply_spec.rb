011821522cd316bac075cead30071cb2efce55ad
WhatsARanjit <whatsaranjit@gmail.com>
2017-11-09 22:40:48 +0000

    Can't rely on test system's filesystem; using notify

--- spec/unit/checks/apply_spec.rb
+++ spec/unit/checks/apply_spec.rb
@@ -15,8 +15,8 @@ describe 'apply check' do
   context 'with an output check' do
     subject = Magicbox::Checks::Apply.new(
       {
-        'code'  => 'file%20%7B%20%27/etc/motd%27%3A%0A%20%20content%20%3D%3E%20%22Hello%20world%5Cn%22%2C%0A%7D',
-        'check' => 'File%5C%5B%5C/etc%5C/motd%5C%5D.*d41d8cd98f00b204e9800998ecf8427e',
+        'code'  => 'notify%20%7B%20%27hello%20world%27%3A%20%7D',
+        'check' => 'changed%20to%20%27hello%20world%27',
       }
     )
 
