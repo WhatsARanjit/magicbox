af55b4aed5652fdf4d93e5df8676928179aa3be0
WhatsARanjit <whatsaranjit@gmail.com>
2017-11-12 15:19:33 +0000

    Updated tests with constants

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,14 @@
+# Constants
 PROJECT_ROOT = File.expand_path('..', File.dirname(__FILE__))
-
+LIB_ROOT     = File.join(PROJECT_ROOT, 'lib', 'magicbox')
 module Magicbox; end
 
-[
-  %w[checks.rb],
-  %w[webserver.rb]
-].each do |file|
-  require File.join(PROJECT_ROOT, 'lib', 'magicbox', *file)
+%w[
+  webserver.rb
+  checks/base.rb
+  spec_tests/base.rb
+].each do |lib|
+  require File.join(LIB_ROOT, lib)
 end
 
 RSpec.configure do |c|
