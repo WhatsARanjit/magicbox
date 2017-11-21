ef33ec4c6825020997d493b28bc2a8708ecd899e
WhatsARanjit <whatsaranjit@gmail.com>
2017-11-21 12:50:57 +0000

    Added unit test for facts endpoint

---
+++ spec/unit/checks/facts.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+output = '{"exitcode":0,"message":["Linux"]}'
+
+subject = Magicbox::Checks::Facts.new(
+  {
+    'fact'  => 'kernel',
+  }
+)
+
+describe 'facts check' do
+  it 'should pass' do
+    expect(subject.parse.to_json).to eql(output)
+  end
+end
