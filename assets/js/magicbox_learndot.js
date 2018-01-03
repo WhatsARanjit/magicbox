0ab863c2f9733310e48a2c21c38cfc24f4886e1b
WhatsARanjit <whatsaranjit@gmail.com>
2018-01-03 23:48:50 +0000

    Native SSL instead of needing an nginx proxy

---
+++ assets/js/magicbox_learndot.js
@@ -0,0 +1,162 @@
+function makeHTMLfriendly(input) {
+  formatted = Array();
+
+  // Split multiline outputs to array elements for easy formatting
+  for (line in input) {
+    if (typeof input[line] == 'string') {
+      formatted = formatted.concat(input[line].split("\n"))
+    } else {
+      formatted = formatted.push(input[line])
+    };
+  };
+  return formatted;
+}
+
+function resultSuccess(res) {
+  console.log(res);
+  formatted = makeHTMLfriendly(res['message']);
+
+  // Optional munging
+  if ( typeof munge_success == 'function' ) {
+    console.log('munge_success triggered');
+    formatted = munge_success(formatted);
+  }
+
+  // Do status box
+  $('#output').attr('rows', formatted.length+2);
+  $('#output').val(formatted.join("\n"));
+
+  // Success colors
+  $('#status').removeClass('text-danger');
+  $('#status').removeClass('text-warning');
+  $('#status').addClass('text-success');
+  $('#status').html('success');
+  $('div.next-requirement').show();
+
+  // Success modal
+  $('#successModal').modal('show');
+  setTimeout(function() { $('#successModal').modal('hide') }, 2000);
+}
+
+function resultError(xhr) {
+  res = JSON.parse(xhr.responseText);
+  console.log(res);
+  formatted = makeHTMLfriendly(res['message']);
+
+  // Optional munging
+  if ( typeof munge_error == 'function' ) {
+    console.log('munge_error triggered');
+    formatted = munge_error(formatted);
+  }
+
+  // Do status box
+  $('#output').attr('rows', formatted.length+2);
+  $('#output').val(formatted.join("\n"));
+
+  // Fail colors
+  $('#status').removeClass('text-success');
+  $('#status').removeClass('text-warning');
+  $('#status').addClass('text-danger');
+  $('#status').html('Something didn\'t work...check the error message below for more details');
+}
+
+function resultComplete(xhr) {
+  res = JSON.parse(xhr.responseText);
+  if (!res['message'].length > 0) {
+    $('#samp_output').hide('fast');
+  } else {
+    $('#samp_output').show('fast');
+  }
+}
+
+function resultWorking() {
+  // Reset box
+  $('#status').removeClass('text-success');
+  $('#status').removeClass('text-danger');
+  $('#status').addClass('text-warning');
+  $('#status_output').show('fast');
+  $('#samp_output').hide('fast');
+  $('#status').html('processing...');
+}
+
+function fakeFail(msg) {
+  resultWorking();
+
+  stuff = '{ "message": ["' + msg  + '"] }';
+  xhr = new Object;
+  xhr.responseText = stuff;
+
+  resultError(xhr);
+  resultComplete(xhr);
+}
+
+function submitcode(endpoint, data, host = '')
+  {
+    resultWorking();
+
+    // Make it pretty for logging/debugging
+    console.log(data.replace(/ {2,}/g, ' '));
+    console.log($('#magic-box').val());
+
+    // Submit API request
+    $.ajax({ 
+      type: 'post',
+      url: host + '/api/1.0/' + endpoint,
+      data: data,
+      dataType:'json',
+      success: function(res) { resultSuccess(res) },
+      error: function(xhr) { resultError(xhr) },
+      complete: function(xhr) { resultComplete(xhr) }
+  });
+}
+
+function formFooter() {
+  output_html = "\
+  <p><button onclick='test();' class='btn btn-warning'>Test</button></p>\
+<form id='status_output' hidden>\
+  <div class='form-inline'>\
+    <label for='output'>Status:</label> <span id='status'>success</span>\
+  </div>\
+  <div class='form-group' id='samp_output'>\
+    <samp><textarea class='form-control' id='output' rows='5' readonly></textarea></samp>\
+  </div>\
+</form>\
+<div id='successModal' class='modal fade bs-example-modal-sm' tabindex='-1' role='dialog' aria-labelledby='mySmallModalLabel'>\
+  <div class='modal-dialog modal-sm' role='document'>\
+    <div class='modal-content'>\
+      <div class='modal-body alert alert-invert'><center>\
+        <h2><img src='https://magicbox.whatsaranjit.com/png/illo-award-achieve_100x100.png' alt='trophy'>\
+        <br>Success!</h2>\
+      </center></div>\
+    </div>\
+  </div>\
+</div>";
+  document.write(output_html);
+}
+
+function catchEnter(box = '#magic-box') {
+  $(box).keypress(function(e) {
+    if (e.which == '13') {
+      e.preventDefault();
+      test();
+      return false
+    }
+  });
+}
+
+function inputCheck(check, box = '#magic-box') {
+  $('button').attr('disabled', 'disabled');
+  $(box).keyup(function(e) {
+    check_regex = new RegExp(check);
+    if (check_regex.exec($(box).val())) {
+      $('button').removeAttr('disabled');
+    }
+    else {
+      $('button').attr('disabled', 'disabled');
+    }
+  });
+}
+
+$(document).ready(function() {
+  $('div.next-requirement').hide();
+});
--- magic.rb
+++ magic.rb
@@ -1,4 +1,6 @@
 require 'yaml'
+require 'webrick'
+require 'webrick/https'
 
 # Constants
 PROJECT_ROOT = __dir__
@@ -16,6 +18,18 @@ end
 # Read config
 @config = YAML.load_file('config.yaml')
 @web    = Magicbox::Webserver.new(@config)
+
+# Webserver options
+w_opts                      = @config['webserver']
+@woptions                   = { app: MyApp }
+@woptions[:Host]            = w_opts['host'] if w_opts.key?('host')
+@woptions[:Port]            = w_opts['port'] if w_opts.key?('port')
+@woptions[:Logger]          = w_opts['logger'] if w_opts.key?('logger')
+@woptions[:SSLEnable]       = w_opts['ssl_enable'] if w_opts.key?('ssl_enable')
+@woptions[:SSLVerifyClient] = w_opts['ssl_verify_client'] if w_opts.key?('ssl_verify_client')
+@woptions[:SSLCertName]     = w_opts['ssl_certname'] if w_opts.key?('ssl_certname')
+@woptions[:SSLCertificate]  = OpenSSL::X509::Certificate.new(File.read(w_opts['ssl_certificate'])) if w_opts.key?('ssl_certificate')
+@woptions[:SSLPrivateKey]   = OpenSSL::PKey::RSA.new(File.read(w_opts['ssl_private_key'])) if w_opts.key?('ssl_private_key')
 
 # Embed pages for iframes
 @config['embedded_pages'].each do |endpoint|
@@ -53,4 +67,5 @@ end
   api_check(endpoint, options['checks'], options['merge'])
 end
 
-@web.run!
+# @web.run!
+Rack::Server.start @woptions
